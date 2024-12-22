96d286646728a1307ab18902b9a7ef155ee6b82e
Bhargav <127436471+Bhargavsq@users.noreply.github.com>
2024-12-23 04:22:04 +0530

    Add files via upload

---
+++ DatabaseDemo/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DatabaseDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <asp:GridView ID="gvEmp" runat="server" />
+            <br />
+            <asp:DropDownList ID="ddlEmpId" runat="server" />
+            <br />
+            Emp ID: <asp:TextBox ID="txtEmpid" runat="server"></asp:TextBox>
+            <br />
+            Emp Name: <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+               <br />
+            Emp City: <asp:TextBox ID="txtEmpCity" runat="server"></asp:TextBox>
+               <br />
+            Emp Salary: <asp:TextBox ID="txtEmpSalary" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DatabaseDemo/WebForm1.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace DatabaseDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillGrid();
+                FillDDL();
+            }
+        }
+
+        private void FillDDL()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LabWork\NET\DatabaseDemo\EmployeeDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT EMPID FROM EMPTABLE";
+
+            con.Open();
+            SqlDataReader dr= cmd.ExecuteReader();
+            ddlEmpId.Items.Clear();
+            while (dr.Read())
+            {
+                ddlEmpId.Items.Add(dr[0].ToString());
+            }
+                  
+
+
+            con.Close();
+        
+        }
+
+        private void FillGrid()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LabWork\NET\DatabaseDemo\EmployeeDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * from EmpTable";
+
+            con.Open();
+            SqlDataReader dr= cmd.ExecuteReader();
+            gvEmp.DataSource = dr;
+            gvEmp.DataBind();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                
+                con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LabWork\NET\DatabaseDemo\EmployeeDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "Insert into EmpTable values(@eid,@ename,@ecity,@esal)";
+                cmd.Parameters.AddWithValue("@eid", txtEmpid.Text);
+                cmd.Parameters.AddWithValue("@ename", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@ecity", txtEmpCity.Text);
+                cmd.Parameters.AddWithValue("@esal", txtEmpSalary.Text);
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+
+
+                if (i > 0)
+                {
+                    Response.Write("Record Inserted");
+                    FillGrid();
+                    FillDDL();
+                    txtEmpid.Text = "";
+                    txtEmpName.Text = "";
+                    txtEmpCity.Text = "";
+                    txtEmpSalary.Text = "";
+
+                }
+                else
+                {
+                    Response.Write("Something went wrong");
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Response.Write(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LabWork\NET\DatabaseDemo\EmployeeDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "Delete from EmpTable where EmpID=@eid";
+                cmd.Parameters.AddWithValue("@eid", txtEmpid.Text);
+              
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+
+
+                if (i > 0)
+                {
+                    Response.Write("Record Deleted");
+                    FillGrid();
+                    FillDDL();
+                    txtEmpid.Text = "";
+                    txtEmpName.Text = "";
+                    txtEmpCity.Text = "";
+                    txtEmpSalary.Text = "";
+
+                }
+                else
+                {
+                    Response.Write("Something went wrong");
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Response.Write(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
